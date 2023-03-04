616502e613462fb75edfc577d794b3b7098412f9
LazyJoshua <568178051@qq.com>
2023-03-04 11:47:57 +0800

    turbo flash message

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -15,7 +15,7 @@ class QuotesController < ApplicationController
     if @quote.save
       respond_to do |format|
         format.html { redirect_to quotes_path, notice: "Quote was successfully created." }
-        format.turbo_stream
+        format.turbo_stream { flash.now[:notice] = "Quote was successfully created." }
       end
     else
       render :new, status: :unprocessable_entity
@@ -33,7 +33,7 @@ class QuotesController < ApplicationController
     if @quote.update(quote_params)
       respond_to do |format|
         format.html { redirect_to quotes_path, notice: "Quote was successfully updated." }
-        format.turbo_stream
+        format.turbo_stream { flash.now[:notice] = "Quote was successfully updated." }
       end
     else
       render :edit, status: :unprocessable_entity
@@ -44,7 +44,7 @@ class QuotesController < ApplicationController
     @quote.destroy
     respond_to do |format|
       format.html { redirect_to quotes_path, notice: "Quote was successfully destroyed." }
-      format.turbo_stream
+      format.turbo_stream { flash.now[:notice] = "Quote was successfully destroyed." }
     end
   end
 
